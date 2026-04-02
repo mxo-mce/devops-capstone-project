03df0b5ed276a512e6e6cef595812b528f7fe6bd
“mxo-mce” <me.mcephe@icloud.com>
2026-04-02 19:33:16 -0400

    Completed Task 5 and achieved 96 percent coverage

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -138,4 +138,7 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
-        
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.put(BASE_URL, json={"name": "not allowed"})
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)    
